10af2cbfe2c032b82904a81f725600274cc823f7
Ben Augarten <b.augarten@gmail.com>
2016-08-29 12:14:26 -0600

    update doc on migration

--- migrations/20160829120855_fix_team_id_in_clips.js
+++ migrations/20160829120855_fix_team_id_in_clips.js
@@ -1,17 +1,23 @@
 
+/**
+ * This migration was needed because heroku didn't have a team_id column in the clips table
+ * Adding a temporary migration just for heroku seemed to make the most sense.
+ *
+ * This column should get added as part of a previous migration.
+ */
 exports.up = function(knex, Promise) {
   return Promise.all([
-    knex.schema.table('clips', function(table) {
-      table.uuid('team_id').notNullable().references('id').inTable('teams');
-    })
+    //knex.schema.table('clips', function(table) {
+    //  table.uuid('team_id').notNullable().references('id').inTable('teams');
+    //})
   ]);
 };
 
 exports.down = function(knex, Promise) {
   return Promise.all([
-    knex.schema.table('clips', function(table) {
-      table.dropColumn('team_id');
-    })
+    //knex.schema.table('clips', function(table) {
+    //  table.dropColumn('team_id');
+    //})
   ])
 };
 
